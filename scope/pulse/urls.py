d9417b223975cda7234499b33e9b9c368880eaac
Taylor Mitchell <taylor.mitchell@gmail.com>
2011-12-15 15:36:29 -0800

    Create a pulse urlconf

--- scope/pulse/urls.py
+++ scope/pulse/urls.py
@@ -1,10 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.views.generic.simple import direct_to_template
 
-from django.contrib import admin
-admin.autodiscover()
-
 urlpatterns = patterns('',
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^pulse/', include('pulse.urls')),
+    url(r'^$', direct_to_template, {'template': 'index.html'}),
 )
