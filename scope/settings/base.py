3e075fb2b0a7ca80b4a3da50ca52c61e4e276fa8
Taylor Mitchell <taylor.mitchell@gmail.com>
2011-12-15 15:31:40 -0800

    Add skeletal pulse app

--- scope/settings/base.py
+++ scope/settings/base.py
@@ -61,6 +61,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
+
+    'pulse',
 )
 
 # A sample logging configuration. The only tangible logging
