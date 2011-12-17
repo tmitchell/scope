839ad1fb51bb80ed7752219cd91e0286a1162ba2
Taylor Mitchell <taylor.mitchell@gmail.com>
2011-12-17 11:52:51 -0800

    Use django-filter to add a date filter to our timeline

--- scope/pulse/views.py
+++ scope/pulse/views.py
@@ -1,7 +1,19 @@
-from django.views.generic import ListView
+import django_filters
+from django.views.generic.base import TemplateResponseMixin, View
 
 from pulse.models import BlipSet
 
 
-class Timeline(ListView):
-    model = BlipSet
+class BlipSetFilterSet(django_filters.FilterSet):
+    timestamp = django_filters.DateRangeFilter()
+    class Meta:
+        model = BlipSet
+        fields = ['timestamp']
+
+
+class Timeline(View, TemplateResponseMixin):
+    template_name = 'pulse/blipset_list.html'
+
+    def get(self, request, *args, **kwargs):
+        context = { 'filter' : BlipSetFilterSet(request.GET, queryset=BlipSet.objects.all()) }
+        return self.render_to_response(context)
