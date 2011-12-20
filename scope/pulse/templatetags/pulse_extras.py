51b53a2f05b51e58779347b1068e3900b0724637
Taylor Mitchell <taylor.mitchell@gmail.com>
2011-12-19 18:48:07 -0800

    Store the timestamp into a variable rather than displaying it outright
    This will let us work with it in the template rather than just echoing it

--- scope/pulse/templatetags/pulse_extras.py
+++ scope/pulse/templatetags/pulse_extras.py
@@ -5,14 +5,15 @@ from django import template
 register = template.Library()
 
 
-class FormatTimelineNode(template.Node):
-    """Displays a given timestamp if the day has changed, according to the specified format"""
+class GetDailyTimestampNode(template.Node):
+    """Stores a given timestamp if the day has changed"""
 
-    def __init__(self, timestamp, format_string):
+    def __init__(self, timestamp, context_var_name):
         self.timestamp = template.Variable(timestamp)
-        self.format_string = format_string
+        self.context_var_name = context_var_name
 
     def render(self, context):
+        context[self.context_var_name] = ''
         try:
             # reduce the timestamp to just year/month/date
             timestamp = self.timestamp.resolve(context)
@@ -21,18 +22,21 @@ class FormatTimelineNode(template.Node):
             if self in context.render_context and ymd_timestamp <= context.render_context[self]:
                 return ''
             context.render_context[self] = ymd_timestamp
-            return ymd_timestamp.strftime(self.format_string)
+            context[self.context_var_name] = ymd_timestamp
+            return ''
         except template.VariableDoesNotExist:
             return ''
 
 
-@register.tag(name='format_timeline')
-def do_format_timeline(parser, token):
+@register.tag(name='get_daily_timestamp')
+def get_daily_timestamp(parser, token):
     try:
         # split_contents() knows not to split quoted strings.
-        tag_name, timestamp, format_string = token.split_contents()
+        tag_name, timestamp, as_string, context_var_name = token.split_contents()
     except ValueError:
-        raise template.TemplateSyntaxError("%r tag requires exactly two arguments" % token.contents.split()[0])
-    if not (format_string[0] == format_string[-1] and format_string[0] in ('"', "'")):
-        raise template.TemplateSyntaxError("%r tag's argument should be in quotes" % tag_name)
-    return FormatTimelineNode(timestamp, format_string[1:-1])
+        raise template.TemplateSyntaxError("%r tag requires exactly three arguments" % token.contents.split()[0])
+    if as_string != 'as':
+        raise template.TemplateSyntaxError("Usage: %s timestamp as varname" % tag_name)
+    if context_var_name[0] == context_var_name[-1] and context_var_name[0] in ('"', "'"):
+        raise template.TemplateSyntaxError("%r tag's varname argument should not be in quotes" % tag_name)
+    return GetDailyTimestampNode(timestamp, context_var_name)
