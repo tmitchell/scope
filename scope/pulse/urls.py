720d3af896b59e7ce33e90b5373d73b8bd03276b
Taylor Mitchell <taylor.mitchell@gmail.com>
2011-12-19 17:15:54 -0800

    Link up views for BlipSets with a given tag

--- scope/pulse/urls.py
+++ scope/pulse/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
-from django.views.generic import ListView, DetailView, TemplateView
+from django.views.generic import DetailView, TemplateView
+from taggit.views import tagged_object_list
 
 from pulse.models import BlipSet
 from pulse.views import Timeline
@@ -9,4 +10,5 @@ urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='index.html')),
     url(r'^timeline$', Timeline.as_view(), name='timeline'),
     url(r'^(?P<slug>\w+)$', DetailView.as_view(model=BlipSet, slug_field='pk'), name='blipset_detail'),
+    url(r'^tags/(?P<slug>\w+)$', tagged_object_list, {'queryset' : BlipSet.objects.all()}, name='blipset_tags'),
 )
