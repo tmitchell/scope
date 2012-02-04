db813c6aaeb1ccbf077973145be5375d8f05b713
Taylor Mitchell <taylor.mitchell@gmail.com>
2012-02-04 11:01:54 -0800

    Add a view for a single blip

--- scope/pulse/urls.py
+++ scope/pulse/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import patterns, include, url
 from django.views.generic import DetailView, TemplateView
 from taggit.views import tagged_object_list
 
-from pulse.models import BlipSet
+from pulse.models import BlipSet, Blip
 from pulse.views import Timeline
 
 
@@ -10,5 +10,6 @@ urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='index.html')),
     url(r'^timeline$', Timeline.as_view(), name='timeline'),
     url(r'^(?P<slug>\w+)$', DetailView.as_view(model=BlipSet, slug_field='pk'), name='blipset_detail'),
+    url(r'^blip/(?P<slug>\w+)$', DetailView.as_view(model=Blip, slug_field='pk'), name='blip_detail'),
     url(r'^tags/(?P<slug>\w+)$', tagged_object_list, {'queryset' : BlipSet.objects.all()}, name='blipset_tags'),
 )
