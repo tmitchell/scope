bbc58b0a6395c1d9f6293eabd21c375271ebb7e9
Taylor Mitchell <taylor.mitchell@gmail.com>
2012-02-04 10:36:37 -0800

    Shield password field in admin form

--- scope/pulse/admin.py
+++ scope/pulse/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
 
+from pulse import forms
 from pulse.models import (BlipSet, RSSProvider, FlickrProvider, BambooBuildsProvider,
                           KunenaProvider, FileSystemChangeProvider, GoogleDocsProvider)
 
+
+class PasswordModelAdmin(admin.ModelAdmin):
+    """Uses custom form to shield a password field"""
+    form = forms.PasswordModelForm
+
+
 # Blips, etc
 admin.site.register(BlipSet)
+
+
+
 
 # Providers
 admin.site.register(RSSProvider)
@@ -12,4 +22,4 @@ admin.site.register(FlickrProvider)
 admin.site.register(BambooBuildsProvider)
 admin.site.register(KunenaProvider)
 admin.site.register(FileSystemChangeProvider)
-admin.site.register(GoogleDocsProvider)
+admin.site.register(GoogleDocsProvider, PasswordModelAdmin)
--- scope/pulse/forms.py
+++ scope/pulse/forms.py
@@ -6,3 +6,11 @@ class TagFilterForm(forms.Form):
     tags = forms.ModelMultipleChoiceField(queryset=Tag.objects.all().order_by('name'),
                                           help_text="Hold Ctrl/Cmd to select multiple",
                                           required=False)
+
+
+class PasswordModelForm(forms.ModelForm):
+    """Simple form to shield a password field"""
+    class Meta:
+        widgets = {
+            'password': forms.PasswordInput,
+        }
