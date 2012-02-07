c7eb356dfcc5168c4d7878684aac71af1529c90d
Taylor Mitchell <taylor.mitchell@gmail.com>
2012-02-06 17:19:25 -0800

    Rework forms for new bootstrap library

--- scope/pulse/forms.py
+++ scope/pulse/forms.py
@@ -2,7 +2,10 @@ from django import forms
 from taggit.models import Tag
 
 
-class TagFilterForm(forms.Form):
+from bootstrap.forms import BootstrapForm
+
+
+class TagFilterForm(BootstrapForm):
     tags = forms.ModelMultipleChoiceField(queryset=Tag.objects.all().order_by('name'),
                                           help_text="Hold Ctrl/Cmd to select multiple",
                                           required=False)
