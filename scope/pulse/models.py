8480c77d829fe52961c593555059a0efeb9d1961
Taylor Mitchell <taylor.mitchell@gmail.com>
2012-03-16 10:51:42 -0700

    Cleanup display

--- scope/pulse/models.py
+++ scope/pulse/models.py
@@ -319,11 +319,11 @@ class GoogleDocsProvider(Provider):
             for revision in revision_atom.entries:
                 timestamp = datetime.datetime.fromtimestamp(time.mktime(revision.updated_parsed)).replace(tzinfo=utc)
                 if timestamp > self.last_update:
-                    #import pdb; pdb.set_trace()
+#                    import pdb; pdb.set_trace()
                     blip = Blip()
                     #  some revisions don't have an author, use the document owner in that case
                     blip.who = getattr(revision, 'author', resource_atom.author)
-                    blip.title = resource_atom.title,
+                    blip.title = resource_atom.title
                     blip.summary="%(title)s edited" % resource_atom
                     blip.source_url = resource_atom.link
                     blip.timestamp = timestamp
